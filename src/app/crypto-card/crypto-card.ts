73efb992d34631f125fbb0b5ceba817b0dfb8728
Paweł Koliweszka <pawelkoliweszka@gmail.com>
2026-02-11 22:03:36 +0100

    Dodanie interakcji poprzez przycisk kup oraz styli za pomocą ngClass

--- src/app/crypto-card/crypto-card.ts
+++ src/app/crypto-card/crypto-card.ts
@@ -1,12 +1,20 @@
 import {Component, Input} from '@angular/core';
 import {CryptoCurrency} from '../models';
+import {CurrencyPipe, NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-crypto-card',
-  imports: [],
+  imports: [
+    CurrencyPipe,
+    NgClass
+  ],
   templateUrl: './crypto-card.html',
   styleUrl: './crypto-card.scss',
 })
 export class CryptoCard {
   @Input() crypto!: CryptoCurrency;
+
+  buyCrypto() {
+    console.log("Kupiono:", this.crypto.name);
+  }
 }
